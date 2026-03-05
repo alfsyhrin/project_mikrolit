647f4ced7375d0fce8cbec284fb45199dfe7bf97
alfsyhrin <alfisyahrinah87@gmail.com>
2026-03-06 00:17:05 +0700

    push konfigurasi database ke Railway Server

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,7 +4,16 @@ const db = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT
+});
+
+db.connect((err) => {
+    if (err) {
+        console.error("Database connection failed:", err);
+    } else {
+        console.log("Database connected");
+    }
 });
 
 module.exports = db;
