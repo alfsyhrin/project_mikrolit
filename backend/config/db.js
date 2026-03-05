1bfe513c2a3867b0aa314123e91ac6f835315ab1
alfsyhrin <alfisyahrinah87@gmail.com>
2026-03-06 01:02:47 +0700

    perbaikan kembali db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,17 +5,9 @@ const db = mysql.createPool({
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
-    port: process.env.DB_PORT || 3306,
-    connectionLimit: 10
+    port: process.env.DB_PORT || 3306
 });
 
-db.getConnection((err, connection) => {
-    if (err) {
-        console.error("Database connection failed:", err);
-    } else {
-        console.log("Database connected successfully");
-        connection.release();
-    }
-});
+console.log("Database Pool Create");
 
 module.exports = db;
