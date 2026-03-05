f5ddab5cb7e7b47b914a4497a50d43d6e135618f
alfsyhrin <alfisyahrinah87@gmail.com>
2026-03-06 01:21:31 +0700

    perbaikan db.js kesekian kalinya

--- backend/config/db.js
+++ backend/config/db.js
@@ -18,4 +18,9 @@ db.getConnection((err, connection) => {
     }
 });
 
+console.log("DB_HOST:", process.env.DB_HOST);
+console.log("DB_USER:", process.env.DB_USER);
+console.log("DB_NAME:", process.env.DB_NAME);
+console.log("DB_PORT:", process.env.DB_PORT);
+
 module.exports = db;
