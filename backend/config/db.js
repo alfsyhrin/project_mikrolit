bd307b5b543cd8a219261f4906491fee03c66ac1
alfsyhrin <alfisyahrinah87@gmail.com>
2026-03-06 01:13:21 +0700

    push perbaikan db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -9,5 +9,9 @@ const db = mysql.createPool({
 });
 
 console.log("Database Pool Create");
+console.log("DB_HOST:", process.env.DB_HOST);
+console.log("DB_USER:", process.env.DB_USER);
+console.log("DB_NAME:", process.env.DB_NAME);
+console.log("DB_PORT:", process.env.DB_PORT);
 
 module.exports = db;
