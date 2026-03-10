2f3db5f907919dc49623373fc5ad6dedbfa7c4cf
alfsyhrin <alfisyahrinah87@gmail.com>
2026-03-11 02:50:07 +0700

    PUSH PERUBAHAN API

--- frontend/assets/api.js
+++ frontend/assets/api.js
@@ -1,4 +1,4 @@
-const API_BASE = "https://erica-slatier-neoma.ngrok-free.dev/api";
+const API_BASE = "https://beige-rabbit-276408.hostingersite.com/api";
 
 //AUTH===================================================================================
 export async function loginRequest(emailOrNidn, password){
