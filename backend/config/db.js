c115832618715778f2f32d512d5faf9344f4bfef
alfsyhrin <alfisyahrinah87@gmail.com>
2026-03-11 05:27:25 +0700

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,6 +1,6 @@
 const mysql = require("mysql2");
 const db = mysql.createPool({
-    host: process.env.DB_HOST || "localhost",
+    host: "127.0.0.1",
     port: Number(process.env.DB_PORT || 3306),
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
